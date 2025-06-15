9ac64c40efcb233660880a729b5db2af0141b2af
AporiaDev <169740552+AporiaDev@users.noreply.github.com>
2025-06-14 19:49:59 -0500

    Complete_One

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,7 +1,36 @@
+import java.util.ArrayList;
+
 public class FizzBuzz {
 
     public static void main(String[] args) {
+        
+        ArrayList<Object> list = new ArrayList<>();
 
-    }
+        for (int i = 1; i <= 100; i++){
+            list.add(i);
+        }
+
+        for (int j = 0; j < list.size(); j++){
+
+            int valor = (int) list.get(j);
+
+            if (valor % 3 == 0){
+                list.set(j, "Fizz");
+                if(valor % 5 == 0){
+                    list.set(j, "FizzBuzz");
+                 }
+            }else if(valor % 5 == 0){
+                list.set(j, "Buzz");
+                if(valor % 3 == 0){
+                    list.set(j, "FizzBuzz");
+                 }
+            }
+        }
+
+        for(Object data : list){
+            System.out.print(data + "\n");
+        }
     
+    }
+
 }
