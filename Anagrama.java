5fb1bd6f878690fbd939fcd26746e2b62d2abd9b
AporiaDev <169740552+AporiaDev@users.noreply.github.com>
2025-06-16 20:41:34 -0500

    Final anagrama v2

--- Anagrama.java
+++ Anagrama.java
@@ -5,8 +5,6 @@ public class Anagrama {
     public static void main(String arg[]){
 
         Scanner scanner = new Scanner(System.in);
-        List<String> letter = new ArrayList<>();
-        List<String> letterTwo = new ArrayList<>();
 
         System.out.print("Input firts word:");
         String wordOne = scanner.nextLine();
@@ -14,46 +12,60 @@ public class Anagrama {
         System.out.print("Input second word:");
         String wordTwo = scanner.nextLine();
 
-        //En Java, == no compara el contenido de las cadenas, sino sus referencias en memoria. Para comparar el contenido de dos String, debes usar:
-        if (wordOne.equals(wordTwo)){
-            System.out.println(false);
+        if (anagramaTrueFalse(wordOne, wordTwo)){
+            System.out.println("Es un Anagrama");
         }else{
-
-            //toCharArray()
-            for (char data : wordOne.toCharArray()){
-                letter.add(String.valueOf(data));
-        }
-            for (char data : wordTwo.toCharArray()){
-                letterTwo.add(String.valueOf(data));
-        }
-        int aux = 0;
-        for(int i = 0; i < letter.size(); i++){
-            for(int j =0; j < letterTwo.size(); j++){
-                if (letter.get(i).equals(letterTwo.get(j))){
-                    aux += 1;
-                    break;
-                }else{
-                    continue;
-                }
-            }
-        }
-        if (aux == 4){
-            System.out.println(true);
-        }else if(aux < 4){
-            System.out.println(false);
-        }else{
-            System.err.println("Value aux exceeds");
-        }
-           
+            System.out.println("No es un Anagrama");
         }
 
         //Una vez que cierras el Scanner, no puedes volver a usar System.in con otro Scanner en el mismo programa. Eso puede causar errores como java.util.NoSuchElementException
         scanner.close();
 
-        
-        //(!wordOne.equals(wordTwo))
+        }
+        public static boolean anagramaTrueFalse(String wordOne , String wordTwo){
+
+            List<String> letter = new ArrayList<>();
+            List<String> letterTwo = new ArrayList<>();
+            String[] alfabeto = {
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+            };
+            List<String> newLetter = new ArrayList<>();
+            List<String> newLetterTwo = new ArrayList<>();
+
+            //toCharArray()
+            for (char data : wordOne.toCharArray()){
+                letter.add(String.valueOf(data));
+            }
+            for (char data : wordTwo.toCharArray()){
+                    letterTwo.add(String.valueOf(data));
+            }
+            //En Java, == no compara el contenido de las cadenas, sino sus referencias en memoria. Para comparar el contenido de dos String, debes usar:
+            if (letter.size() != letterTwo.size()){
+                return false;
+                
+            }else if(letter.equals(letterTwo)){
+                return false;
+            }
+            else{
+                for(int i = 0; i < letter.size(); i++){
+                    for(int j =0; j < letter.size(); j++){
+                        if (alfabeto[i].equals(letterTwo.get(j))){
+                            newLetterTwo.add(alfabeto[i]);
+                        }else if((alfabeto[i].equals(letter.get(j)))){
+                            newLetter.add(alfabeto[i]);
+                        }else{
+                            continue;
+                        }
+                    }
+                }
+                return newLetter.equals(newLetterTwo);
+
+            }
+        }
+                //(!wordOne.equals(wordTwo))
  
 
     }
     
-}
+
