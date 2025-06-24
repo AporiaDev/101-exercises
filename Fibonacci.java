c307aa0b8ff28aae8bfdd6105e7ead4bcde282a9
AporiaDev <169740552+AporiaDev@users.noreply.github.com>
2025-06-23 21:03:08 -0500

    commit

---
+++ Fibonacci.java
@@ -0,0 +1,13 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 50; i++){
+            
+            int suport = 0;
+            System.out.println(suport);
+            suport += 1;
+
+        } 
+    }
+    
+}
