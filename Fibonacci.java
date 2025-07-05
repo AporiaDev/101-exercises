bae4a9624531bfbc56bd61723f37f5fbd4b00e4f
AporiaDev <169740552+AporiaDev@users.noreply.github.com>
2025-07-04 21:49:36 -0500

    Update FIbo

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,13 +1,21 @@
+import java.util.*;
+
 public class Fibonacci {
     public static void main(String[] args) {
         
-        for (int i = 0; i < 50; i++){
-            
-            int suport = 0;
-            System.out.println(suport);
-            suport += 1;
 
-        } 
+        int valueFibonacci = 0;
+        int aux = 0;
+        int aux2 = 1;
+
+        for (int i = 0; i < 50;i++){
+
+            System.out.println(valueFibonacci);
+            valueFibonacci = aux2 + aux;
+            aux2 = aux;
+            aux = valueFibonacci;
+        }
+        
     }
     
 }
